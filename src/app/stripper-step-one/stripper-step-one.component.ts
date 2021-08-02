bbd494b5c7543f40778aba0bb0d02cb0761ba5ce
DESKTOP-1D2DO70\Albiorix Technology <diptikanta.albiorix@gmail.com>
2021-08-02 14:37:15 +0530

    complated my project

--- src/app/stripper-step-one/stripper-step-one.component.ts
+++ src/app/stripper-step-one/stripper-step-one.component.ts
@@ -34,21 +34,8 @@ export class StripperStepOneComponent implements OnInit {
 
 // Display image on new tab
   const fileupload = ($event.target as HTMLInputElement).files[0]; 
-  console.log('fileupload',fileupload)
   let  src=URL.createObjectURL(fileupload);
   window.open(src);
-
-    // console.log('cxbv', $event)
-    // const fileupload = ($event.target as HTMLInputElement).files[0];
-    // console.log('fdgf', fileupload);
-
-    //  var reader:any = new FileReader();
-    //  reader.onload = function () {
-    // localStorage.setItem('images', JSON.stringify(reader.result));
-
-      //  reader.src=URL.createObjectURL($event.target.files[0]);
-      // window.open('C:\Users\Albiorix Technology\Pictures\Saved Pictures','_blank' );
-      // console.log('dfsdg',reader.result)
     }
    
 }
